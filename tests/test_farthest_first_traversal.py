1c6644e007dca2b1ca0611c70c2cc85e56d71994
braceal <braceal@umich.edu>
2020-08-13 18:23:12 -0400

    added basic test

---
+++ tests/test_farthest_first_traversal.py
@@ -0,0 +1,17 @@
+import unittest
+import numpy as np
+from ApproximateGreedyPermutation.algorithms import farthest_first_traversal
+
+class Test_farthest_first_traversal(unittest.TestCase):
+
+    def test_basic(self):
+        data = np.array([[1,2], [5,6], [0,0]])
+        k_farthest = farthest_first_traversal(data, k=2)
+
+        self.assertTrue(np.array([0,0]) in k_farthest)
+        self.assertTrue(np.array([5,6]) in k_farthest)
+        self.assertEqual(k_farthest.shape, (2, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
